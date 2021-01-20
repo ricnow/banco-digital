5373dfcd00a6abd31b6daa83435e2f9ae9cbd473
Richelle Soares <richelle.soares@gmail.com>
2021-01-20 09:24:21 -0300

    Finzalização da Tela Home

--- src/screen/Home/index.js
+++ src/screen/Home/index.js
@@ -4,6 +4,7 @@ import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons'
 import Suggestions from '../../components/Suggestions';
 import Activities from '../../components/Activities';
 import Tips from '../../components/Tips';
+import Banner from '../../components/Banner';
 
 export default function Home() {
     return (
@@ -20,7 +21,9 @@ export default function Home() {
                 <Suggestions />
                 <Activities />
                 <Tips />
+                <Banner />
             </Container>
+
         </Wrapper>
     );
 }
